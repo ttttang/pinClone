e364720251d4c7eaebbce555e043f1c1beb6705a
ttttang <tracyt.tang@gmail.com>
2014-10-24 14:54:03 -0700

    Finished adding Firebase & Importing JSON data

--- controllers/angularController.js
+++ controllers/angularController.js
@@ -1,18 +1,20 @@
-var pinApp = angular.module("pinterest", []);
+var pinApp = angular.module("pinterest", ["firebase"]); //making firebase a dependency for my module//
+
 
 // <!-- to display each post!-->
 
-pinApp.controller('pinController', function($scope){
-	$scope.tracy=posts;
+pinApp.controller('mailman1', function($scope, $firebase){
+	var postRef = new Firebase('https://pinclone.firebaseio.com/posts'); //connect my controller to the firebase data-storage//
+	var sync = $firebase(postRef); // create object out of firebase data
+	$scope.tracy=sync.$asArray(); //change firebase object into array
 
 
 // to add a new post //
 
 $scope.addPost = function(x, y, z, a, b){
-			var aedit=b.split(" , ");
+			var bedit=b.split(" , ");
 
-			$scope.tracy.push({image_url:x, goal:y, importance:z, time:a, category:bedit})
-		
+			$scope.tracy.$add({image_url:x, goal:y, importance:z, time:a, category:bedit});		
 			$scope.pics="";
 			$scope.goal="";
 			$scope.importance="";
@@ -26,16 +28,8 @@ $scope.addPost = function(x, y, z, a, b){
 $scope.deletePost=function(postName){
 			var check = confirm("Are You Sure You Want To Delete This Post??");
 			if(check==true){
-			$scope.tracy.splice(postName, 1);}
+			$scope.tracy.$remove(postName);
+			}
 		}
-
-
-
-
-
-
-
-
-
 
 })
